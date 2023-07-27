e4cbc0621091876db0143c7a01dc8272faaf3036
Andrey Dumskiy <dumskiy-25@mail.ru>
2023-07-27 21:31:09 +0300

    Adds fixes

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,22 @@
 let navMain = document.querySelector('.main-header__navigation');
 let navToggle = document.querySelector('.main-header__toggle');
+let mapInteractive = document.querySelector('.map__interactive');
+let mapImage = document.querySelector('.map__image');
 
 navMain.classList.remove('main-header__navigation--nojs');
 navToggle.classList.remove('main-header__toggle--nojs');
+mapInteractive.classList.remove('map__interactive--nojs');
+mapImage.classList.remove('map__image--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-header__navigation--closed')) {
+  if (navToggle.classList.contains('main-header__toggle--closed')) {
+    navToggle.classList.remove('main-header__toggle--closed');
+    navToggle.classList.add('main-header__toggle--opened');
     navMain.classList.remove('main-header__navigation--closed');
     navMain.classList.add('main-header__navigation--opened');
   } else {
+    navToggle.classList.add('main-header__toggle--closed');
+    navToggle.classList.remove('main-header__toggle--opened');
     navMain.classList.add('main-header__navigation--closed');
     navMain.classList.remove('main-header__navigation--opened');
   }
